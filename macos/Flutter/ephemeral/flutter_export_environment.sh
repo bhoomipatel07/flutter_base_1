fdcc6f655dd4b317822ad20b1ddd89091432b57f
Bhoomi Patel <bhoomi.p@masterlysolutions.com>
2025-04-24 11:10:45 +0530

    Updated git flow file and iOS fastfile

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/masetlysolutions/fvm/versions/3.29.0"
-export "FLUTTER_APPLICATION_PATH=/Users/masetlysolutions/Documents/Flutter Projects/flutter_base_code"
+export "FLUTTER_ROOT=/Users/masetlysolutions/Documents/development/flutter_sdk/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/masetlysolutions/Documents/Flutter Projects/flutter_base_1"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
